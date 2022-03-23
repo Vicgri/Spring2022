f0dd3e473610837df69462b5f04154767957078c
Vicgri <Victoria.grimen@gmail.com>
2022-03-23 16:26:26 +0100

    Oppgave6

--- src/Week04/Program.java
+++ src/Week04/Program.java
@@ -7,7 +7,8 @@ public class Program {
     public void runProgram() {
 
         System.out.println("Program starting");
-        oppgave4();
+        // oppgave4();
+        oppgave6();
 
     }
 
@@ -24,6 +25,29 @@ public class Program {
             list) {
             System.out.println(s);
         }
+
+    }
+
+
+    private void oppgave6() {
+        Scanner input = new Scanner(System.in);
+        int number = 0;
+        ArrayList<Integer> numbers = new ArrayList<>();
+        System.out.println("Skriv inn et par tall. Avslutt med negative -tall");
+
+        while (number>=0) {
+            number = input.nextInt();
+            if(number>=0) {
+                numbers.add(number);
+            }
+        }
+
+        int sum = 0;
+        for (Integer i :
+            numbers) {
+            sum+=i;
+        }
+        System.out.println("Sum:"+sum);
 
     }
 }
