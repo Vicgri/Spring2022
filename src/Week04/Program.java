c8a080ab48545799f3400c9827c8cb282d2ff54e
Vicgri <Victoria.grimen@gmail.com>
2022-03-23 16:17:24 +0100

    Oppgave4

--- src/Week04/Program.java
+++ src/Week04/Program.java
@@ -1,9 +1,29 @@
 package Week04;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Program {
     public void runProgram() {
 
         System.out.println("Program starting");
+        oppgave4();
+
+    }
+
+    private void oppgave4() {
+        ArrayList<String> list = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Skriv inn tre ord");
+        list.add(input.next());
+        list.add(input.next());
+        list.add(input.next());
+        System.out.println("Du skrev inn: ");
+
+        for (String s:
+            list) {
+            System.out.println(s);
+        }
 
     }
 }
